b5772593558a6b4c8ff6edda38cc33870c77320e
Yusuf Söyler <yusufsoyler5222@gmail.com>
2023-12-20 00:14:54 +0300

    v1

---
+++ is_takip_proje/is_takip_proje/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace is_takip_proje
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnDepartmanListesi_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Formlar.FrmDepartmanlar frm = new Formlar.FrmDepartmanlar();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void BtnPersonel_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Formlar.FrmPersoneller frm2 = new Formlar.FrmPersoneller();
+            frm2.MdiParent = this;
+            frm2.Show();
+        }
+
+        private void BtnPersonelİstatistik_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Formlar.FrmPersonelİstatistik frm3 = new Formlar.FrmPersonelİstatistik();
+            frm3.MdiParent = this;
+            frm3.Show();
+        }
+
+        private void BtnGorevListesi_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Formlar.FrmGorevListesi frm4 = new Formlar.FrmGorevListesi();
+            frm4.MdiParent = this;
+            frm4.Show();
+        }
+    }
+}
